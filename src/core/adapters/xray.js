f2f305008cf6b4be467e3a5857b7a7da6fb35495
dukangalex <128732965+dukangalex@users.noreply.github.com>
2026-09-22 07:32:48 -0700

    fix(xray): avoid unsupported Hysteria2 bandwidth fields

--- src/core/adapters/xray.js
+++ src/core/adapters/xray.js
@@ -65,7 +65,7 @@ function buildStreamSettings(node) {
     const tls = node.tls;
     const reality = node.reality;
     const out = {};
-    if (node.protocol === 'hysteria2') {\n        out.network = 'hysteria';\n        out.method = 'hysteria';\n        out.hysteriaSettings = { version: 2, auth: node.credentials?.password, up: node.protocolOptions?.up, down: node.protocolOptions?.down };\n    } else if (t.type) {
+    if (node.protocol === 'hysteria2') {\n        out.network = 'hysteria';\n        out.method = 'hysteria';\n        out.hysteriaSettings = { version: 2, auth: node.credentials?.password };\n    } else if (t.type) {
         const network = t.type === 'ws' ? 'ws' : t.type === 'grpc' ? 'grpc' : t.type === 'httpupgrade' ? 'httpupgrade' : t.type === 'mkcp' ? 'kcp' : t.type === 'xhttp' ? 'xhttp' : 'tcp';
         out.network = network;
         if (network === 'ws') out.wsSettings = pick(t, ['path', 'headers']);
