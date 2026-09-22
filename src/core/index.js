36df3d6dbbdb21f93068ec7f079a7c69b198c944
dukangalex <128732965+dukangalex@users.noreply.github.com>
2026-09-22 06:45:30 -0700

    feat: expose Clash adapter

--- src/core/index.js
+++ src/core/index.js
@@ -4,3 +4,4 @@ export { createProxyNode } from './ProxyNode.js';
 export { normalizeProxy, normalizeProxies } from './normalizeProxy.js';
 export { declareCapability, canConvert, explainConversion, getCapabilityMatrix, getNodeFeatures } from './capabilityMatrix.js';
 export { toSingBox } from './adapters/singbox.js';
+export { toClash } from './adapters/clash.js';
