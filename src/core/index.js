e71f71cb860895cbfadcdae3b0afb9bf61054f71
dukangalex <128732965+dukangalex@users.noreply.github.com>
2026-09-22 06:32:33 -0700

    feat(core): export canonical core API

---
+++ src/core/index.js
@@ -0,0 +1,3 @@
+export { createProxyNode } from './ProxyNode.js';
+export { normalizeProxy, normalizeProxies } from './normalizeProxy.js';
+export { declareCapability, canConvert, explainConversion, getCapabilityMatrix } from './capabilityMatrix.js';
