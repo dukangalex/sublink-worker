0396023180efa553918ea5245c428fbe1b5ab5bb
dukangalex <128732965+dukangalex@users.noreply.github.com>
2026-09-22 06:34:31 -0700

    feat(core): expose sing-box adapter

--- src/core/index.js
+++ src/core/index.js
@@ -1,3 +1,4 @@
 export { createProxyNode } from './ProxyNode.js';
 export { normalizeProxy, normalizeProxies } from './normalizeProxy.js';
 export { declareCapability, canConvert, explainConversion, getCapabilityMatrix } from './capabilityMatrix.js';
+export { toSingBox } from './adapters/singbox.js';
