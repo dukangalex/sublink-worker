d8b2fa0eaa90558408ffe745553cd4915896f2be
dukangalex <128732965+dukangalex@users.noreply.github.com>
2026-09-22 06:38:44 -0700

    fix: initialize core adapter capabilities

--- src/core/index.js
+++ src/core/index.js
@@ -1,4 +1,6 @@
+import './adapters/index.js';
+
 export { createProxyNode } from './ProxyNode.js';
 export { normalizeProxy, normalizeProxies } from './normalizeProxy.js';
-export { declareCapability, canConvert, explainConversion, getCapabilityMatrix } from './capabilityMatrix.js';
+export { declareCapability, canConvert, explainConversion, getCapabilityMatrix, getNodeFeatures } from './capabilityMatrix.js';
 export { toSingBox } from './adapters/singbox.js';
